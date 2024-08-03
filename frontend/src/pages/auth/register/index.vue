9692b5cbe49ce4b4340f7698e6521f960ffad31e
joao5142 <joaopauloneto3687@gmail.com>
2024-08-03 20:51:35 -0300

    feat: create register view

---
+++ frontend/src/pages/auth/register/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login d-flex flex-column w-100">
+    <app-text as="strong" size="xl" color="text-900">Cadastre sua conta</app-text>
+    <app-text as="span" size="sm" color="text-900" class="d-block mt-1"
+      >Registre sua conta parar ajudar outras pessoas.</app-text
+    >
+
+    <app-input class="mt-10" type="text" placeholder="E-mail" />
+    <app-input class="mt-3" type="text" placeholder="Senha" />
+    <app-input class="mt-3" type="text" placeholder="Senha" />
+    <app-input class="mt-3" type="text" placeholder="Repetir Senha" />
+
+    <app-button glow class="mt-10" full @click="$router.push('/home')" prefetch
+      >Cadastrar</app-button
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  name: 'Login',
+  layout: 'auth',
+})
+useHead({
+  title: 'Login',
+})
+</script>
+
+<style scoped lang="scss">
+.login {
+  &__image {
+    max-width: 150px;
+  }
+}
+</style>
