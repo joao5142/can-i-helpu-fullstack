3f1f3d842a8d6a48637b3529d9d05f14ce8a9d57
joao5142 <joaopauloneto3687@gmail.com>
2024-08-03 20:40:32 -0300

    feat: add auth layout and init login view

---
+++ frontend/src/layouts/auth.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="auth">
+      <div>
+        <img class="auth__image" :src="BannerImage" alt="Banner" />
+      </div>
+
+      <div class="auth__content">
+        <slot></slot>
+      </div>
+    </div>
+    <app-loading v-if="isLoading" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BannerImage from '@/assets/img/background.webp'
+
+const { isLoading } = useLoadingIndicator()
+</script>
+
+<style scoped lang="scss">
+.auth {
+  display: grid;
+  grid-template-columns: 1fr 500px;
+
+  height: 100vh;
+  overflow-y: hidden;
+
+  &__image {
+    width: 100%;
+    max-height: 100vh;
+    height: 100%;
+
+    object-fit: cover;
+  }
+
+  &__content {
+    padding: 5rem;
+    flex: 1;
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    justify-content: center;
+  }
+
+  @media (max-width: 700px) {
+    &__image {
+      display: none;
+    }
+    & {
+      grid-template-columns: 1fr;
+    }
+
+    &__content {
+      padding: 2rem;
+    }
+  }
+}
+</style>
---
+++ frontend/src/pages/auth/login/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login d-flex flex-column w-100">
+    <app-text class="mt-10" weight="bold" as="span" size="4xl" color="text-900" family="secondary"
+      >Can I HelpU</app-text
+    >
+
+    <app-text class="mt-10" as="span" size="lg" color="text-900">Bem vindo</app-text>
+    <app-text as="strong" size="xl" color="text-900">Entre na sua conta</app-text>
+
+    <app-input class="mt-10" type="text" placeholder="E-mail" />
+    <app-input class="mt-3" type="text" placeholder="Senha" />
+
+    <app-text
+      as="span"
+      class="text-end mt-2 cursor-pointer"
+      weight="medium"
+      size="sm"
+      color="text-900"
+      ><NuxtLink to="/auth/register" prefetch class="link">Esqueceu sua senha ?</NuxtLink></app-text
+    >
+
+    <app-button glow class="mt-10" full @click="$router.push('/home')" prefetch>Entrar</app-button>
+
+    <app-text class="mt-10 text-center" size="sm" as="span" color="text-700">
+      Ainda não possui Conta?
+      <app-text color="primary" as="strong"
+        ><NuxtLink to="/auth/register" prefetch class="link">Cadastre-se</NuxtLink></app-text
+      >
+    </app-text>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  name: 'Login',
+  layout: 'auth',
+})
+useHead({
+  title: 'Login',
+})
+</script>
+
+<style scoped lang="scss">
+.login {
+  &__image {
+    max-width: 150px;
+  }
+}
+</style>
