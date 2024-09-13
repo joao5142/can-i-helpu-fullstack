5a7fc3af233ffb8f08644ddcbb39359c49427f7e
joao5142 <joaopauloneto3687@gmail.com>
2024-09-12 22:41:02 -0300

    refactor: adjust monorepo config

--- apps/frontend/eslint.config.ts
+++ apps/frontend/eslint.config.ts
@@ -1,49 +1,10 @@
 export default {
-  parser: "vue-eslint-parser",
-  parserOptions: {
-    parser: "@typescript-eslint/parser",
-  },
+  root: true,
   extends: [
-    "@can-i-helpu/config/eslint.config.mjs",
+    "@can-i-helpu/eslint-config/vue/eslint.config.ts",
     "@nuxtjs/eslint-config-typescript",
-    "plugin:vue/vue3-recommended",
-    "plugin:prettier/recommended",
-    "plugin:storybook/recommended",
   ],
-  rules: {
-    "vue/multi-word-component-names": "off",
-    "vue/valid-v-slot": [
-      "error",
-      {
-        allowModifiers: true,
-      },
-    ],
-    "vue/html-self-closing": [
-      "error",
-      {
-        html: {
-          void: "always",
-          normal: "never",
-          component: "any",
-        },
-        svg: "always",
-        math: "always",
-      },
-    ],
-    "vue/html-closing-bracket-newline": [
-      "error",
-      {
-        singleline: "never",
-        multiline: "always",
-      },
-    ],
-    "vue/component-name-in-template-casing": [
-      "error",
-      "kebab-case",
-      {
-        registeredComponentsOnly: false,
-        ignores: [],
-      },
-    ],
+  parserOptions: {
+    project: true,
   },
 };
---
+++ packages/eslint-config/vue/eslint.config.ts
@@ -0,0 +1,48 @@
+export default {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: [
+    "../eslint.default.config.ts",
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
+    "plugin:storybook/recommended",
+  ],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "vue/valid-v-slot": [
+      "error",
+      {
+        allowModifiers: true,
+      },
+    ],
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "never",
+          component: "any",
+        },
+        svg: "always",
+        math: "always",
+      },
+    ],
+    "vue/html-closing-bracket-newline": [
+      "error",
+      {
+        singleline: "never",
+        multiline: "always",
+      },
+    ],
+    "vue/component-name-in-template-casing": [
+      "error",
+      "kebab-case",
+      {
+        registeredComponentsOnly: false,
+        ignores: [],
+      },
+    ],
+  },
+};
