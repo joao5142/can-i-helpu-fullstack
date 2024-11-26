ba49bd183db00ab1c5801d8782eaf8737e400ff6
joao5142 <joaopauloneto3687@gmail.com>
2024-11-25 23:18:43 -0300

    refactor: adjust app port to the env variable

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
+import { EnvType } from "./env";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
-  const port = configService.get("PORT");
+  const configService = app.get<ConfigService<EnvType, true>>(ConfigService);
+  const port = configService.get("PORT", { infer: true });
 
   await app.listen(port);
 }
