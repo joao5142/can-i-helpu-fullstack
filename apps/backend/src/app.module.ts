ad2a9efc4deb276594dc28dcc18cfbfb4aa4806e
joao5142 <joaopauloneto3687@gmail.com>
2024-11-25 23:06:22 -0300

    feat: add global app config service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PrismaService } from "./prisma/prisma.service";
+import { envSchema } from "./env";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
