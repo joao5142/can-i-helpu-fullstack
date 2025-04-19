39359e3f82f344cf9aa92d5ceda24fc0a32aeef1
joao5142 <joaopauloneto3687@gmail.com>
2025-04-18 22:17:59 -0300

    chore: adjust entrypoint

--- apps/backend/entrypoint.sh
+++ apps/backend/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-
-until nc -z "${DB_HOST:-localhost}" "${DB_PORT:-5432}"; do
-  echo "Aguardando o banco de dados"
-  sleep 2
-done
-
-
 pnpm dlx prisma migrate deploy
 
 exec node dist/main
